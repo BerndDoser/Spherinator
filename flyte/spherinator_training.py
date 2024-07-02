a02e5f047d04d586a840caf2eeb9e8ae869c73bb
Bernd Doser <bernd.doser@h-its.org>
2024-07-02 16:35:25 +0200

    Add spherinator training workflow

---
+++ flyte/spherinator_training.py
@@ -0,0 +1,103 @@
+import os
+
+import lightning as L
+from flytekit import ImageSpec, PodTemplate, Resources, task, workflow
+from flytekit.extras.accelerators import T4
+from flytekit.types.directory import FlyteDirectory
+from kubernetes.client.models import (
+    V1Container,
+    V1EmptyDirVolumeSource,
+    V1PodSpec,
+    V1Volume,
+    V1VolumeMount,
+)
+
+from spherinator.data import IllustrisSdssDataModule
+from spherinator.models import RotationalVariationalAutoencoderPower
+
+custom_image = ImageSpec(
+    packages=[
+        "flytekitplugins-kfpytorch",
+        "kubernetes",
+        "spherinator",
+    ],
+    # use the cuda and python_version arguments to build a CUDA image
+    cuda="12.1.0",
+    python_version="3.10",
+    registry="registry.h-its.org/doserbd/flyte",
+)
+
+
+container = V1Container(
+    name=custom_image.name,
+    volume_mounts=[V1VolumeMount(mount_path="/dev/shm", name="dshm")],
+)
+volume = V1Volume(name="dshm", empty_dir=V1EmptyDirVolumeSource(medium="Memory"))
+custom_pod_template = PodTemplate(
+    primary_container_name=custom_image.name,
+    pod_spec=V1PodSpec(
+        runtime_class_name="nvidia", containers=[container], volumes=[volume]
+    ),
+)
+
+
+@task(
+    container_image=ImageSpec(
+        packages=["minio"],
+        registry="registry.h-its.org/doserbd/flyte",
+    ),
+)
+def get_data() -> FlyteDirectory:
+    from minio import Minio
+
+    data_dir = os.path.join(os.getcwd(), "data")
+    os.makedirs(data_dir, exist_ok=True)
+
+    client = Minio("minio-api-itssv197.h-its.org", secure=False)
+
+    for item in client.list_objects("illustris", recursive=True):
+        client.fget_object(
+            "illustris", item.object_name, os.path.join(data_dir, item.object_name)
+        )
+
+    return FlyteDirectory(path=str(data_dir))
+
+
+@task(
+    container_image=custom_image,
+    requests=Resources(mem="32Gi", cpu="48", gpu="1", ephemeral_storage="100Gi"),
+    pod_template=custom_pod_template,
+)
+def train_model(data_dir: FlyteDirectory) -> FlyteDirectory:
+
+    model = RotationalVariationalAutoencoderPower()
+
+    data_dir.download()
+    datamodule = IllustrisSdssDataModule(
+        [str(data_dir.path) + "/two-images/TNG100/sdss/snapnum_099/data"]
+    )
+    datamodule.setup("fit")
+
+    model_dir = os.path.join(os.getcwd(), "model")
+    trainer = L.Trainer(
+        default_root_dir=model_dir,
+        max_epochs=100,
+        # strategy="ddp",
+        # precision="16-mixed",
+        # accelerator="gpu",
+        inference_mode=False,
+    )
+
+    trainer.fit(model=model, datamodule=datamodule)
+    return FlyteDirectory(path=str(model_dir))
+
+
+@workflow
+def wf() -> FlyteDirectory:
+    data_dir = get_data()
+    model_dir = train_model(data_dir=data_dir)
+    return model_dir
+
+
+if __name__ == "__main__":
+    print(f"Running workflow() {wf()}")
