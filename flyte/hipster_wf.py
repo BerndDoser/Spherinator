c141cc349a62549430a483a523b2b41552a458af
Bernd Doser <bernd.doser@h-its.org>
2024-07-25 16:23:04 +0200

    read model and parameters into flyte task

--- flyte/hipster_wf.py
+++ flyte/hipster_wf.py
@@ -1,7 +1,11 @@
+import importlib
 import os
 
+import torch
+import yaml
 from flytekit import ImageSpec, PodTemplate, Resources, task, workflow
 from flytekit.types.directory import FlyteDirectory
+from flytekit.types.file import FlyteFile
 from kubernetes.client.models import (
     V1Container,
     V1EmptyDirVolumeSource,
@@ -11,7 +15,6 @@ from kubernetes.client.models import (
 )
 
 from hipster import Hipster
-from spherinator.models import RotationalVariationalAutoencoderPower
 
 custom_image = ImageSpec(
     packages=[
@@ -45,9 +48,23 @@ custom_pod_template = PodTemplate(
     requests=Resources(mem="32Gi", cpu="48", gpu="1", ephemeral_storage="100Gi"),
     pod_template=custom_pod_template,
 )
-def generate_hips() -> FlyteDirectory:
+def generate_hips(config_file: FlyteFile, checkpoint_file: FlyteFile) -> FlyteDirectory:
 
-    model = RotationalVariationalAutoencoderPower()
+    with open(config_file, "r", encoding="utf-8") as stream:
+        config = yaml.load(stream, Loader=yaml.Loader)
+
+    model_class_path = config["model"]["class_path"]
+    module_name, class_name = model_class_path.rsplit(".", 1)
+    module = importlib.import_module(module_name)
+    model_class = getattr(module, class_name)
+    model_init_args = config["model"]["init_args"]
+    model = model_class(**model_init_args)
+
+    # Load the parameters from the checkpoint file
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    checkpoint = torch.load(checkpoint_file, map_location=device)
+    model.load_state_dict(checkpoint["state_dict"])
+    model.eval()
 
     hipster_dir = os.path.join(os.getcwd(), "hipster")
     hipster = Hipster(output_folder=hipster_dir, title="Illustris")
@@ -56,11 +73,20 @@ def generate_hips() -> FlyteDirectory:
 
 
 @workflow
-def wf() -> FlyteDirectory:
-    hipster_dir = generate_hips()
+def wf(config_file: FlyteFile, checkpoint_file: FlyteFile) -> FlyteDirectory:
+    hipster_dir = generate_hips(
+        config_file=config_file, checkpoint_file=checkpoint_file
+    )
     # catalog = generate_catalog()
     return hipster_dir
 
 
 if __name__ == "__main__":
-    print(f"Running workflow() {wf()}")
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument("--config_file", type=str)
+    parser.add_argument("--checkpoint_file", type=str)
+
+    args = parser.parse_args()
+    print(wf(config_file=args.config_file, checkpoint_file=args.checkpoint_file))
