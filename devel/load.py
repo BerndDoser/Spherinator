28b7dbf0e295e4b20dbde7eadd5b35f52646a648
Bernd Doser <bernd.doser@h-its.org>
2025-02-20 13:44:01 +0100

    test hf data loading with streaming support

--- devel/load.py
+++ devel/load.py
@@ -1,16 +1,28 @@
 import torch
 from datasets import load_dataset
 
+# from torchvision import transforms
+
 dataset = load_dataset(
-    "parquet", data_dir="/home/doserbd/data/gaia/xp_calibrated/parquet_subset"
+    "parquet",
+    data_dir="/home/doserbd/data/gaia/xp_calibrated/parquet_subset",
+    streaming=True,
+    # transform=transforms.Compose(
+    #     [
+    #         transforms.ToTensor(),
+    #     ]
+    # ),
 )
 
 print(dataset)
 
 dataloader = torch.utils.data.DataLoader(
-    dataset["train"], batch_size=128, shuffle=True, num_workers=4
+    dataset["train"],
+    batch_size=128,
+    shuffle=False,
+    num_workers=4,
 )
 
-
 batch = next(iter(dataloader))
-print(batch["flux"])
+# assert batch["flux"].shape == (128, 1, 343)
+assert len(batch["flux"]) == 343
